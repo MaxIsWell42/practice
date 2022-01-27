70d8fc3ed6fc081008ed0965fdfff3e0ca6076eb
MaxIsWell42 <maxfin13@gmail.com>
2022-01-27 17:01:37 -0500

    Setup waysToSum

---
+++ python/waysToSum.py
@@ -0,0 +1,12 @@
+# Ways to sum
+# Given positive integers total and k, calculate the number of
+# possible ways to achieve total using numbers 1-k
+# Example: total = 8, k = 2
+#   Output: 5
+#   Explanation:
+#   1,1,1,1,1,1,1,1
+#   1,1,1,1,1,1,2
+#   1,1,1,1,2,2
+#   1,1,2,2,2
+#   2,2,2,2
+
