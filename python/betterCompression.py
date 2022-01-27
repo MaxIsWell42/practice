3e8697ba9fbbddda665a1c75734db66dede109df
MaxIsWell42 <maxfin13@gmail.com>
2022-01-27 18:57:00 -0500

    Attempted, terrible interview problem but might come back regardless

--- python/betterCompression.py
+++ python/betterCompression.py
@@ -5,5 +5,43 @@
 #   Since it has two instances of c, they should be added and the string returned as:
 #   a3c10b2
 
-def betterCompression(s: String):
-    return 
+from posixpath import split
+import re
+
+def betterCompression(s: str):
+    vals = {}
+    res = ""
+    
+    # Split into array of letters and numbers
+    splitList = re.split('(\d+)', s)
+    splitList.pop()
+    # ['a', '3', 'c', '9', 'b', '2', 'c', '1']
+    
+    for i in range(len(splitList)-1):
+        # If the value already exists, add to it by the frequency of this new instance
+        # This never triggers for some reason
+        if splitList[i] in vals:
+            currFreq = splitList[i+1]
+            print("Current Frequency: {}".format(currFreq))
+            print(splitList[i])
+            vals[splitList[i]] += currFreq
+            
+        # If it doesn't, create it
+        # Something in here is messing up, causing all keys to be incremented numbers(1,2,3...)
+        else:
+            vals[i] = splitList[i+1]
+            
+    # Turn the dictionary back into a string
+    for key in vals:
+        print('Current Key: {}'.format(key))
+        print('Current dictionary: {}'.format(vals))
+        res += str(key)
+        res += vals.get(key)
+    
+    
+    
+    return res  
+
+if __name__ == "__main__":
+    s = "a3c9b2c1"
+    print(betterCompression(s))
